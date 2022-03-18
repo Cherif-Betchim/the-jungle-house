f75e403c6c659eecd30e3a59556864db862f9d51
Cherif-Betchim <cherif-betchim@hotmail.com>
2022-03-18 13:41:11 +0100

    wip on cards

--- src/components/CardsLayout.js
+++ src/components/CardsLayout.js
@@ -9,98 +9,101 @@ function CardsLayout() {
 
     return (
         <div>
+            <div className={"col align-self-end"}>
+                <h1 className={"align-self-end"}>hola</h1>
+            </div>
+            {/*<Container >*/}
 
-            <Container>
-                <Row className={"row justify-content-end cardRow"} md={4}>
-                    <Col >
-                        <Card style={{width: '18rem'}}>
-                            <Card.Img variant="top" src={test}/>
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col xs={6}>
+                {/*<Row className={"row justify-content-end cardRow"} md={3}>*/}
+                {/*    <Col >*/}
+                {/*        <Card style={{width: '18rem'}}>*/}
+                {/*            <Card.Img variant="top" src={test}/>*/}
+                {/*            <Card.Body>*/}
+                {/*                <Card.Title>Card Title</Card.Title>*/}
+                {/*                <Card.Text>*/}
+                {/*                    Some quick example text to build on the card title and make up the bulk of*/}
+                {/*                    the card's content.*/}
+                {/*                </Card.Text>*/}
+                {/*                <Button variant="primary">Go somewhere</Button>*/}
+                {/*            </Card.Body>*/}
+                {/*        </Card>*/}
+                {/*    </Col>*/}
+                {/*    <Col xs={6}>*/}
 
-                        <Card style={{width: '18rem'}}>
-                            <Card.Img variant="top" src={test}/>
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                {/*        <Card style={{width: '18rem'}}>*/}
+                {/*            <Card.Img variant="top" src={test}/>*/}
+                {/*            <Card.Body>*/}
+                {/*                <Card.Title>Card Title</Card.Title>*/}
+                {/*                <Card.Text>*/}
+                {/*                    Some quick example text to build on the card title and make up the bulk of*/}
+                {/*                    the card's content.*/}
+                {/*                </Card.Text>*/}
+                {/*                <Button variant="primary">Go somewhere</Button>*/}
+                {/*            </Card.Body>*/}
+                {/*        </Card>*/}
+                {/*    </Col>*/}
 
-                    <Col>
-                        <Card style={{width: '18rem'}}>
-                            <Card.Img variant="top" src={test}/>
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                {/*    <Col>*/}
+                {/*        <Card style={{width: '18rem'}}>*/}
+                {/*            <Card.Img variant="top" src={test}/>*/}
+                {/*            <Card.Body>*/}
+                {/*                <Card.Title>Card Title</Card.Title>*/}
+                {/*                <Card.Text>*/}
+                {/*                    Some quick example text to build on the card title and make up the bulk of*/}
+                {/*                    the card's content.*/}
+                {/*                </Card.Text>*/}
+                {/*                <Button variant="primary">Go somewhere</Button>*/}
+                {/*            </Card.Body>*/}
+                {/*        </Card>*/}
+                {/*    </Col>*/}
 
-                </Row>
-                <Row className={"row justify-content-end cardRow"} md={4}>
-                    <Col >
-                        <Card style={{width: '18rem'}}>
-                            <Card.Img variant="top" src={test}/>
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col xs={6}>
+                {/*</Row>*/}
+                {/*<Row className={"row justify-content-end cardRow"} md={3}>*/}
+                {/*    <Col >*/}
+                {/*        <Card style={{width: '18rem'}}>*/}
+                {/*            <Card.Img variant="top" src={test}/>*/}
+                {/*            <Card.Body>*/}
+                {/*                <Card.Title>Card Title</Card.Title>*/}
+                {/*                <Card.Text>*/}
+                {/*                    Some quick example text to build on the card title and make up the bulk of*/}
+                {/*                    the card's content.*/}
+                {/*                </Card.Text>*/}
+                {/*                <Button variant="primary">Go somewhere</Button>*/}
+                {/*            </Card.Body>*/}
+                {/*        </Card>*/}
+                {/*    </Col>*/}
+                {/*    <Col xs={6}>*/}
 
-                        <Card style={{width: '18rem'}}>
-                            <Card.Img variant="top" src={test}/>
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                {/*        <Card style={{width: '18rem'}}>*/}
+                {/*            <Card.Img variant="top" src={test}/>*/}
+                {/*            <Card.Body>*/}
+                {/*                <Card.Title>Card Title</Card.Title>*/}
+                {/*                <Card.Text>*/}
+                {/*                    Some quick example text to build on the card title and make up the bulk of*/}
+                {/*                    the card's content.*/}
+                {/*                </Card.Text>*/}
+                {/*                <Button variant="primary">Go somewhere</Button>*/}
+                {/*            </Card.Body>*/}
+                {/*        </Card>*/}
+                {/*    </Col>*/}
 
-                    <Col>
-                        <Card style={{width: '18rem'}}>
-                            <Card.Img variant="top" src={test}/>
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                {/*    <Col>*/}
+                {/*        <Card style={{width: '18rem'}}>*/}
+                {/*            <Card.Img variant="top" src={test}/>*/}
+                {/*            <Card.Body>*/}
+                {/*                <Card.Title>Card Title</Card.Title>*/}
+                {/*                <Card.Text>*/}
+                {/*                    Some quick example text to build on the card title and make up the bulk of*/}
+                {/*                    the card's content.*/}
+                {/*                </Card.Text>*/}
+                {/*                <Button variant="primary">Go somewhere</Button>*/}
+                {/*            </Card.Body>*/}
+                {/*        </Card>*/}
+                {/*    </Col>*/}
 
-                </Row>
+                {/*</Row>*/}
 
-            </Container>
+            {/*</Container>*/}
 
         </div>
 
